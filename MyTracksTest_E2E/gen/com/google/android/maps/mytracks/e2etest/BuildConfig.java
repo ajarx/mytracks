938966e8275e7d7ec6f116027054e070f63826c9
tree1224 <mengzhaojun@live.com>
2014-11-23 14:27:46 +0800

    Initial code from google

---
+++ MyTracksTest_E2E/gen/com/google/android/maps/mytracks/e2etest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.maps.mytracks.e2etest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
